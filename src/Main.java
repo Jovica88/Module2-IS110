effbb59feb1d10b3ed7291068eee580e33c8da28
Jovica Ugrinic <jovicu08@uia.no>
2022-01-17 19:03:49 +0100

    ny

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main  {
+    public static void main(String[] args) {
+        int number = 10;
+
+        if (number > 10) {
+            System.out.println("større en 10");
+        }
+        else {
+            System.out.println("10 eller mindre");
+        }
+
+    }
+
+}
+
+
